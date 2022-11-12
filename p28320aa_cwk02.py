157cc7d8993a29fb06ce92995c4541de5af759f3
p28320aa <arefin.ahammed@student.manchester.ac.uk>
2022-11-12 18:10:22 +0000

    Added a docstring and sources of initial images

---
+++ p28320aa_cwk02.py
@@ -0,0 +1,20 @@
+# Coursework 2 of COMP16321
+
+"""
+*** The screen resolution is 1440x900
+
+* This is a classic asteroid game where the player needs to avoid hitting the asteroids to survive.
+* The difficulty increases at certain scores.
+* The speed of the asteroids will increase making the game harder and harder at each level.
+"""
+
+
+# game_icon.ico source: https://www.freeiconspng.com/img/17270
+# spaceship_image.png source: https://www.pngkey.com/detail/u2q8a9t4r5y3a9r5_spaceship-png-file-spaceship-png/
+# asteroid_1.png source: https://www.pngmart.com/image/50759
+# asteroid_2.png source: https://pngimg.com/image/105528
+# asteroid_3.png source: https://pngimg.com/image/105498
+# asteroid_4.png source: https://pngimg.com/image/105494
+# background_image.jpg source: https://phys.org/news/2022-08-ai-space-anomalies.html
+
+
