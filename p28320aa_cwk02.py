2f1a8b0628b305b643f9fde1edec9a0fedca88f0
p28320aa <arefin.ahammed@student.manchester.ac.uk>
2022-11-15 17:47:44 +0000

    Added provisional pause function and scoring system

--- p28320aa_cwk02.py
+++ p28320aa_cwk02.py
@@ -24,13 +24,13 @@ from PIL import Image, ImageTk
 
 
 # Configure main window
-def configure_root():
-    root.title("Asteroid Game")
-    root.iconbitmap("images/game_icon.ico")
-    root.configure()
+def configure_window():
+    window.title("Asteroid Game")
+    window.iconbitmap("images/game_icon.ico")
+    window.configure()
 
     # Disabled resizing of the window
-    root.resizable(False, False)
+    window.resizable(False, False)
 
     """ Fixing geometry so that the window opens at the center """
     # Width and height of the window
@@ -38,15 +38,15 @@ def configure_root():
     height = window_height
 
     # Screen width and height
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
 
     # Change of coordinates
     x = int(screen_width / 2 - width / 2)
     y = int(screen_height / 2 - height / 2 - 20)
 
     # screen position
-    root.geometry(f"{width}x{height}+{x}+{y}")
+    window.geometry(f"{width}x{height}+{x}+{y}")
 
 
 # Creating keybindings to move the spaceship
@@ -74,17 +74,23 @@ def move_spaceship_down(e):
     canvas_main.move(spaceship, x, y)
 
 
+def pause(e):
+    global pause_game
+
+    if pause_game == False:
+        pause_game = True
+
+    elif pause_game == True:
+        pause_game = False
+        # main_game()
+
+
 # Creating main game function
 def main_game():
     """Adding Background to the pain game"""
     # Open background image
     global background_image
     background_image = ImageTk.PhotoImage(Image.open("images/background.jpg"))
-
-    # Creating a Canvas
-    global canvas_main
-    canvas_main = Canvas(root, width=window_width, height=window_height)
-    canvas_main.pack(fill="both", expand=True)
 
     # Add background to canvas
     canvas_main.create_image(0, 0, image=background_image, anchor=NW)
@@ -101,17 +107,14 @@ def main_game():
     y = 750
     spaceship = canvas_main.create_image(x, y, image=spaceship_image, anchor=NW)
 
+    """ Making the scoring system """
+    # storing and displaying the score
+    score = 0
+    score_text = "Score: " + str(score)
+    scoreText = canvas_main.create_text(window_width - window_width/8, window_height/15, fill="white", font=custom_font, text=score_text)
 
-    # Keybindings
-    root.bind("<Left>", move_spaceship_left)
-    root.bind("<Right>", move_spaceship_right)
-    root.bind("<Up>", move_spaceship_up)
-    root.bind("<Down>", move_spaceship_down)
 
-    Label1 = Label(canvas_main, text="Score: 0", font=custom_font, bg="black", fg="white")
-    Label1.place(relx=0.79, rely=0.05)
-
-root = Tk()
+window = Tk()
 
 """Defining variables"""
 # Width and height of the window
@@ -124,7 +127,24 @@ custom_font = Font(
     size=35,
 )
 
-configure_root()
+configure_window()
+
+# Variables
+pause_game = False
+
+""" Creating the Canvas """
+canvas_main = Canvas(window, width=window_width, height=window_height)
+canvas_main.pack(fill="both", expand=True)
+
+
+# Keybindings
+canvas_main.bind("<Left>", move_spaceship_left)
+canvas_main.bind("<Right>", move_spaceship_right)
+canvas_main.bind("<Up>", move_spaceship_up)
+canvas_main.bind("<Down>", move_spaceship_down)
+canvas_main.bind("<Escape>", pause)
+canvas_main.focus_set()
+
 main_game()
 
-root.mainloop()
+window.mainloop()
